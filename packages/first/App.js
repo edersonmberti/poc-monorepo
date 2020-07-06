1fc93b595f8ff06623f7cff859ab9b54c1079c8e
Ederson Machado <edersonmberti@gmail.com>
2020-07-06 18:46:35 -0300

    add project

---
+++ packages/first/App.js
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import { View, Text, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>First - Home Screen</Text>
+      <Button
+        onPress={() => navigation.navigate('FirstProfileScreen')}
+        title="GO TO PROFILE"
+        color="#841584"
+      />
+    </View>
+  );
+}
+
+function ProfileScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>First - Profile Screen</Text>
+      <Button
+        onPress={() => navigation.navigate('FirstHomeScreen')}
+        title="GO TO HOME"
+        color="#841584"
+      />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="FirstHomeScreen" component={HomeScreen} />
+        <Stack.Screen name="FirstProfileScreen" component={ProfileScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ packages/second/App.js
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import { View, Text, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Second - Home Screen</Text>
+      <Button
+        onPress={() => navigation.navigate('SecondProfileScreen')}
+        title="GO TO PROFILE"
+        color="#841584"
+      />
+    </View>
+  );
+}
+
+function ProfileScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Second - Profile Screen</Text>
+      <Button
+        onPress={() => navigation.navigate('SecondHomeScreen')}
+        title="GO TO HOME"
+        color="#841584"
+      />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="SecondHomeScreen" component={HomeScreen} />
+        <Stack.Screen name="SecondProfileScreen" component={ProfileScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
